0a4549fcd164e5daa4a32bf9af75c5cc9746a2a2
omogood <omogood4u@gmail.com>
2018-09-09 13:23:31 +0900

    Clear ボタンを追加

--- src/Example_2.elm
+++ src/Example_2.elm
@@ -1,7 +1,7 @@
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (Html, Attribute, div, button, input, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 
 
 -- MAIN
@@ -23,14 +23,19 @@ init =
 
 -- UPDATE
 
-type Msg
-    = Change String
+-- Change 関数は、String 型の引数を一つ取る
+-- Clear 関数は引数を取らない（定数）
+type Msg = Change String | Clear
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
+        -- 入力された文字列で更新する
         Change newContent ->
             { model | content = newContent }
+        -- 保持している文字列を削除する
+        Clear ->
+            { model | content = "" }
 
 
 -- VIEW
@@ -38,6 +43,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-    [ input [placeholder "Text to reverse", value model.content, onInput Change] []
+    [ input [placeholder "入力された文字を反転", value model.content, onInput Change] []
+    , button [ onClick Clear ] [ text "Clear" ]
     , div [] [text (String.reverse model.content)]
     ]
